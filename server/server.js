63c6d656d1735ecdefd32d37b19cba123536bfc1
xWyvernPx <phonglethanh2@gmail.com>
2022-10-12 10:42:22 +0700

    update production config

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const app = require("./src/app.js");
 const { createServer } = require("http");
 require("dotenv").config();
 require("./database.js");
-const port = process.env.DEV_PORT || 4422;
+const port = process.env.DEV_PORT || 8080;
 const httpServer = createServer(app);
 
 httpServer.listen(port, () => {
--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://www.wyvernp.tech/",
     credentials: true,
   })
 );
